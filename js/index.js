0b5dbcd6532e4984a85904f3a59b5e7b3aff3752
jimwcollins <jimwcollins@gmail.com>
2020-09-17 15:07:22 +0100

    Coded header menu

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@
 *  index.js
 * 
 ***************************/
+
+/**************
+* Site-wide JS
+**************/
+
+// Header 
+
+// Handle header on page scroll
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > (window.innerHeight - 1) || document.documentElement.scrollTop > (window.innerHeight - 1)) {
+        document.querySelector('.header').classList.add('header--scroll');
+        document.querySelectorAll('.nav__link').forEach(item => item.classList.add('nav__link--scroll'));
+    } else {
+        document.querySelector('.header').classList.remove('header--scroll');
+        document.querySelectorAll('.nav__link').forEach(item => item.classList.remove('nav__link--scroll'));
+    }
+});
