e105c1f5cb287930bc33a468981990353cc8f10b
James Collins <85165470+jimcollins-tred@users.noreply.github.com>
2025-01-28 18:04:38 +0000

    Complete Tred spotlight

--- js/animation.js
+++ js/animation.js
@@ -10,43 +10,48 @@
  *******************/
 
 const playVid = (vid, play) => {
-  if (play) vid.play();
-  else vid.pause();
+	console.log('vid', vid);
+	console.log('play', play);
+
+	if (play) vid.play();
+	else vid.pause();
 };
 
 // Single vids
 const allSoloVids = document.querySelectorAll('.video');
 
 const handleSingleVid = ([entry]) => {
-  playVid(entry.target, entry.isIntersecting);
+	playVid(entry.target, entry.isIntersecting);
 };
 
 const vidObserver = new IntersectionObserver(handleSingleVid, {
-  root: null,
-  threshold: 1,
+	root: null,
+	threshold: 1,
 });
 
 allSoloVids.forEach((vid) => {
-  vidObserver.observe(vid);
+	vidObserver.observe(vid);
 });
 
 // Vid Collection
 const allMultVids = document.querySelectorAll('.videoMulti');
 
+console.log('allMultVids', allMultVids);
+
 const handleMultiVids = ([entry]) => {
-  // Loop through all videos in collection and play/pause all
-  [...entry.target.children].forEach((childVid) => {
-    playVid(childVid, entry.isIntersecting);
-  });
+	// Loop through all videos in collection and play/pause all
+	[...entry.target.children].forEach((childVid) => {
+		playVid(childVid, entry.isIntersecting);
+	});
 };
 
 const multiVidObserver = new IntersectionObserver(handleMultiVids, {
-  root: null,
-  threshold: 1,
+	root: null,
+	threshold: 1,
 });
 
 allMultVids.forEach((multi) => {
-  multiVidObserver.observe(multi);
+	multiVidObserver.observe(multi);
 });
 
 /*******************
@@ -56,23 +61,23 @@ allMultVids.forEach((multi) => {
 const allFadeIns = document.querySelectorAll('.fadein');
 
 const revealElem = ([entry], observer) => {
-  if (!entry.isIntersecting) return;
+	if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove('fadein--hidden');
-  observer.unobserve(entry.target);
+	entry.target.classList.remove('fadein--hidden');
+	observer.unobserve(entry.target);
 };
 
 const fadeObserver = new IntersectionObserver(revealElem, {
-  root: null,
-  threshold: 0.65,
+	root: null,
+	threshold: 0.65,
 });
 
 // Only set up observers and hide spreads if user hasn't clicked back or fwd
 if (!browserBtn) {
-  allFadeIns.forEach((elem) => {
-    elem.classList.add('fadein--hidden');
-    fadeObserver.observe(elem);
-  });
+	allFadeIns.forEach((elem) => {
+		elem.classList.add('fadein--hidden');
+		fadeObserver.observe(elem);
+	});
 }
 
 /*************************
@@ -82,33 +87,33 @@ if (!browserBtn) {
 const allSplits = document.querySelectorAll('.section__split');
 
 const revealSplit = ([entry], observer) => {
-  if (!entry.isIntersecting) return;
+	if (!entry.isIntersecting) return;
 
-  // Remove the hidden class when we intersect left and right divs
-  const leftDiv = entry.target.children[0];
-  const rightDiv = entry.target.children[1];
+	// Remove the hidden class when we intersect left and right divs
+	const leftDiv = entry.target.children[0];
+	const rightDiv = entry.target.children[1];
 
-  leftDiv.classList.remove('split__left--hidden');
-  rightDiv.classList.remove('split__right--hidden');
+	leftDiv.classList.remove('split__left--hidden');
+	rightDiv.classList.remove('split__right--hidden');
 
-  observer.unobserve(entry.target);
+	observer.unobserve(entry.target);
 };
 
 const splitObserver = new IntersectionObserver(revealSplit, {
-  root: null,
-  threshold: 0.75,
+	root: null,
+	threshold: 0.75,
 });
 
 if (!browserBtn) {
-  allSplits.forEach((section) => {
-    // Hide section initially
-    const leftDiv = section.children[0];
-    const rightDiv = section.children[1];
-    leftDiv.classList.add('split__left--hidden');
-    rightDiv.classList.add('split__right--hidden');
+	allSplits.forEach((section) => {
+		// Hide section initially
+		const leftDiv = section.children[0];
+		const rightDiv = section.children[1];
+		leftDiv.classList.add('split__left--hidden');
+		rightDiv.classList.add('split__right--hidden');
 
-    splitObserver.observe(section);
-  });
+		splitObserver.observe(section);
+	});
 }
 
 /*************************
@@ -118,32 +123,32 @@ if (!browserBtn) {
 const allPinch = document.querySelectorAll('.animPinch');
 
 const revealPinch = ([entry], observer) => {
-  if (!entry.isIntersecting) return;
+	if (!entry.isIntersecting) return;
 
-  // Remove the hidden class when we intersect left and right divs
-  const leftElem = entry.target.children[0];
-  const rightElem = entry.target.children[1];
+	// Remove the hidden class when we intersect left and right divs
+	const leftElem = entry.target.children[0];
+	const rightElem = entry.target.children[1];
 
-  leftElem.classList.remove('animPinch__left--hidden');
-  rightElem.classList.remove('animPinch__right--hidden');
+	leftElem.classList.remove('animPinch__left--hidden');
+	rightElem.classList.remove('animPinch__right--hidden');
 
-  observer.unobserve(entry.target);
+	observer.unobserve(entry.target);
 };
 
 const pinchObserver = new IntersectionObserver(revealPinch, {
-  root: null,
-  threshold: 0.25,
+	root: null,
+	threshold: 0.25,
 });
 
 if (!browserBtn) {
-  allPinch.forEach((pinch) => {
-    const leftElem = pinch.children[0];
-    const rightElem = pinch.children[1];
-    leftElem.classList.add('animPinch__left--hidden');
-    rightElem.classList.add('animPinch__right--hidden');
+	allPinch.forEach((pinch) => {
+		const leftElem = pinch.children[0];
+		const rightElem = pinch.children[1];
+		leftElem.classList.add('animPinch__left--hidden');
+		rightElem.classList.add('animPinch__right--hidden');
 
-    pinchObserver.observe(pinch);
-  });
+		pinchObserver.observe(pinch);
+	});
 }
 
 /***************************
@@ -153,22 +158,22 @@ if (!browserBtn) {
 const allUpImages = document.querySelectorAll('.animUp');
 
 const revealUpImg = ([entry], observer) => {
-  if (!entry.isIntersecting) return;
+	if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove('animUp--hidden');
-  observer.unobserve(entry.target);
+	entry.target.classList.remove('animUp--hidden');
+	observer.unobserve(entry.target);
 };
 
 const upImgObserver = new IntersectionObserver(revealUpImg, {
-  root: null,
-  threshold: 0.3,
+	root: null,
+	threshold: 0.3,
 });
 
 if (!browserBtn) {
-  allUpImages.forEach((image) => {
-    image.classList.add('animUp--hidden');
-    upImgObserver.observe(image);
-  });
+	allUpImages.forEach((image) => {
+		image.classList.add('animUp--hidden');
+		upImgObserver.observe(image);
+	});
 }
 
 /***************************
@@ -178,27 +183,27 @@ if (!browserBtn) {
 const allSpreads = document.querySelectorAll('.spread');
 
 const revealSpread = ([entry], observer) => {
-  if (!entry.isIntersecting) return;
+	if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove('spread__left--hidden');
-  entry.target.classList.remove('spread__right--hidden');
+	entry.target.classList.remove('spread__left--hidden');
+	entry.target.classList.remove('spread__right--hidden');
 
-  observer.unobserve(entry.target);
+	observer.unobserve(entry.target);
 };
 
 const spreadObserver = new IntersectionObserver(revealSpread, {
-  root: null,
-  threshold: 0.85,
+	root: null,
+	threshold: 0.85,
 });
 
 let isLeft = false;
 
 if (!browserBtn) {
-  allSpreads.forEach((spread) => {
-    isLeft = !isLeft;
-    if (isLeft) spread.classList.add('spread__left--hidden');
-    else spread.classList.add('spread__right--hidden');
+	allSpreads.forEach((spread) => {
+		isLeft = !isLeft;
+		if (isLeft) spread.classList.add('spread__left--hidden');
+		else spread.classList.add('spread__right--hidden');
 
-    spreadObserver.observe(spread);
-  });
+		spreadObserver.observe(spread);
+	});
 }
